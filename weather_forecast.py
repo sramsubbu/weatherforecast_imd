d6531db82fd32d347f2839cbcf80e69f40389627
Ramasubramanian Seetharaman <sramsubu@gmail.com>
2017-09-13 11:32:43 +0530

    get the url of the city dynamically
    Added code to dynamically get the url of the city given the city name

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+from urllib.parse import quote
 from bs4 import BeautifulSoup
 
 def fetch_data_from_url(url):
@@ -29,16 +30,50 @@ def scrap_required_data(soup):
     ty.pop("Local Weather Report and Forecast")
     
     return ty,fcs
-            
 
+
+def get_cities():
+    url = "http://www.imd.gov.in/pages/city_weather_main.php"
+    soup = fetch_data_from_url(url)
+    options = soup.findAll("form")[0].findAll("option")
+    cities = [i.attrs['value'] for i in options]
+    return cities
+
+def get_city_from_user():
+    city_list = get_cities()
+    for ind,val in enumerate(city_list):
+        print(ind,":",val)
+    city = input("Please enter the city from the list: ")
+    while city not in city_list:
+        city = input("Please enter a valid input")
+    return city
+
+def get_city_url(city):
+    city = quote(city)
+    url = "http://www.imd.gov.in/pages/city_weather_main.php"
+    soup = fetch_data_from_url(url)
+    action = soup.findAll("form")[0].attrs['action']
+    data = "obs_name={city}&submit=Go".format(city=city)
+    string = urlopen("http://www.imd.gov.in/pages/city_weather_show.php", data=bytes(data,'utf-8')).read()
+    soup = BeautifulSoup(string,'html.parser')
+    return soup.findAll("iframe")[0].attrs['src']
+    
 
 if __name__ == "__main__":
     # this url is specific for chennai location. For other stations a different url is needed
-    url = "http://city.imd.gov.in/citywx/city_weather.php?id=43279"
+    city4 = "CHENNAI (NUNGAMBAKKAM)"
+    city1 = "NEW DELHI (SAFDARJUNG)"
+    city2 = "MUMBAI (SANTACRUZ) (A)"
+    city3 = "KOLKATA"
+    url = get_city_url(city2)
     soup = fetch_data_from_url(url)
     today,forecast = scrap_required_data(soup)
     for day in forecast:
         print(day[0],"\t",day[3])
+    
+    
+    
+    
         
         
     
